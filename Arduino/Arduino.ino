4b113d7445c488437295b0d48458bbb1dcc1c21b
Kyle Combes <kylecombes@gmail.com>
2018-12-02 03:33:00 -0500

    Integrate facial and buffalo recognition with stamp and gate.

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,4 +1,5 @@
 #include <Adafruit_MotorShield.h>
+#include <Servo.h>
 /*     Simple Stepper Motor Control Exaple Code
  *      
  *  by Dejan Nedelkovski, www.HowToMechatronics.com
@@ -8,20 +9,30 @@
 // defines pins numbers
 #define STEP_PIN 3
 #define DIR_PIN 4
-#define ACCEPT_POS 500
-#define DENY_POS 1000
+#define GATE_SERVO_PIN 11
+#define GANTRY_ACCEPT_POS 500
+#define GANTRY_REJECT_POS 2000
+#define GATE_CLOSED_POS 90
+#define GATE_OPEN_POS 0
+#define GATE_OPEN_MS 10000
 #define STAMP_TRAVEL_MS 1000
 #define STAMP_PAUSE_MS 300
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_DCMotor *stampMotor = AFMS.getMotor(4);
+Adafruit_DCMotor *stampMotor = AFMS.getMotor(1);
+Servo gateServo;
 
-int curPos = 0;
+int curGantryPos = GANTRY_REJECT_POS;
+int curGatePos = GATE_CLOSED_POS;
 
 void setup() {
   // Set up the Adafruit motor shield and stamp DC motor
   AFMS.begin();
   stampMotor->setSpeed(150);
+
+  // Set up the gate servo
+  gateServo.attach(GATE_SERVO_PIN);
+  gateServo.write(GATE_CLOSED_POS);
   
   // Sets the two pins as Outputs
   pinMode(STEP_PIN, OUTPUT); 
@@ -33,11 +44,11 @@ void setup() {
 void loop() {
   // Look for command from the computer
   while (Serial.available() > 0) {
-    String cmd = Serial.readString();
-    if (cmd == "accept") {
+    char cmd = Serial.read();
+    if (cmd == 'a') {
       Serial.println("Stamping seal of approval.");
       acceptApplication();
-    } else if (cmd == "reject") {
+    } else if (cmd == 'r') {
       Serial.println("Rejecting application.");
       rejectApplication();
     } else {
@@ -46,38 +57,50 @@ void loop() {
       Serial.print(cmd);
       Serial.println("\"");
     }
+    // Clear the serial buffer in case there were multiple commands sent
+    // TODO: Fix this kiosk-side
+    Serial.flush();
   }
 }
 
 void rejectApplication() {
   // Move the gantry
-  moveToPos(DENY_POS);
+  moveToPos(GANTRY_REJECT_POS);
 
   // Actuate the stamp
+  Serial.println("Stamping application");
   stamp();
 }
 
 void acceptApplication() {
   // Move the gantry
-  moveToPos(ACCEPT_POS);
+  moveToPos(GANTRY_ACCEPT_POS);
 
   // Actuate the stamp
+  Serial.println("Stamping application");
   stamp();
+
+  // Open the gate for the desired amount of timme
+  Serial.println("Opening gate");
+  setGatePos(GATE_OPEN_POS);
+  delay(GATE_OPEN_MS);
+  Serial.print("Closing gate");
+  setGatePos(GATE_CLOSED_POS);
 }
 
 void moveToPos(int pos) {
   // Determine and set the direction
-  int direction = (curPos > pos) ? HIGH : LOW;
-  int step = (curPos < pos) ? 1 : -1;
+  int direction = (curGantryPos > pos) ? HIGH : LOW;
+  int step = (curGantryPos < pos) ? 1 : -1;
   digitalWrite(DIR_PIN, direction);
 
   // Move the gantry
-  while (curPos != pos) {
+  while (curGantryPos != pos) {
     digitalWrite(STEP_PIN, HIGH);
     delayMicroseconds(500);
     digitalWrite(STEP_PIN, LOW);
     delayMicroseconds(500);
-    curPos += step;
+    curGantryPos += step;
   }
 }
 
@@ -89,4 +112,13 @@ void stamp() {
   stampMotor->run(BACKWARD);
   delay(STAMP_TRAVEL_MS);
   stampMotor->run(RELEASE);
+}
+
+void setGatePos(int pos) {
+  int step = (curGatePos > pos) ? -1 : 1;
+  while (curGatePos != pos) {
+    gateServo.write(curGatePos);
+    curGatePos += step;
+    delay(10);
+  }
 }
--- kiosk.py
+++ kiosk.py
@@ -5,6 +5,7 @@ import sys
 sys.path = [p for p in sys.path if 'ros' not in p]
 import time
 import cv2
+from arduino_comm import ArduinoComm
 from FacialRecognition.identify_face import FaceIdentifier
 from BuffaloHat.buffalo_detect import detectHat
 from QRScan.scan import decode
@@ -18,6 +19,7 @@ class Kiosk:
         self.qr_code_camera = cv2.VideoCapture(qr_code_camera_id) if qr_code_camera_id is not None else None
         self.mugshot_camera = cv2.VideoCapture(mugshot_camera_id) if mugshot_camera_id is not None else None
         self.face_identifier = FaceIdentifier(facial_recognition_model_path)
+        self.arduino_comm = ArduinoComm()
 
     def check_for_qr_code(self):
         # Grab a frame from the QR code camera
@@ -41,9 +43,18 @@ class Kiosk:
 
         return names_in_frame, wearing_buffalo_hat
 
-    def check_application_with_server(self, application_id, name, is_wearing_hat):
+    def check_application_with_server(self, application_id, names, is_wearing_hat):
+        print('Checking with server')
         # TODO: Send the data to the server
-        raise NotImplementedError()
+        # Temporary behavior:
+        if 'Kyle_Combes' in names or is_wearing_hat:
+            print('verified')
+            self.arduino_comm.approve()
+            time.sleep(20)
+        else:
+            print('reject')
+            self.arduino_comm.reject()
+            time.sleep(5)
 
     def handle_server_response(self, response):
         raise NotImplementedError()
@@ -58,6 +69,7 @@ class Kiosk:
                     names_in_frame, wearing_hat = self.process_image()
                     print('Names in frame:', names_in_frame)
                     print('Wearing hat:', wearing_hat)
+                    self.check_application_with_server(application_id, names_in_frame, wearing_hat)
 
             time.sleep(0.5)
 
